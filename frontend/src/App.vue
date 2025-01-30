7dcda8c02a9d3ff960869d3872685fe72948afc7
hrk17 <hannahkubk@gmail.com>
2025-01-30 16:53:01 -0500

    upload modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import HomePage from './pages/HomePage.vue';
+
 
 </script>
 
@@ -27,7 +29,7 @@
     </nav>
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
+        <HomePage></HomePage>
       </div>
     </main>
   </div>
