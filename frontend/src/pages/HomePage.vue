2a47170db49295b1493da342f4f93e67583d3929
hrk17 <hannahkubk@gmail.com>
2025-01-30 17:05:43 -0500

    dropbox styling

--- frontend/src/pages/HomePage.vue
+++ frontend/src/pages/HomePage.vue
@@ -25,11 +25,11 @@ const handleFileUpload = async (event: Event) => {
 
 <template>
     <button class="text-white bg-rose" @click="showUploadModal = true">Upload new audio file</button>
+    <!-- Begin dropbox -->
     <form v-if="showUploadModal" class="pt-2">
-
         <div class="flex items-center justify-center w-full">
             <label for="dropzone-file"
-                class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                class="flex flex-col items-center justify-center w-full my-3 pt-2 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
                 <div class="flex flex-col items-center justify-center pt-5 pb-6">
                     <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
@@ -41,8 +41,16 @@ const handleFileUpload = async (event: Event) => {
                     <p class="text-xs text-gray-500 dark:text-gray-400">Audio files only</p>
                 </div>
                 <input id="dropzone-file" type="file" class="hidden" accept="audio/*" @change="handleFileUpload" />
+                <button @click.stop="showUploadModal = false"
+                    class="bg-rose justify-left text-white rounded-md text-sm hover:bg-rose-800" style="width: 100%;">
+                    ✖ Cancel
+                </button>
             </label>
         </div>
-
     </form>
+    <!-- End dropbox -->
+
+    <card>
+        hi
+    </card>
 </template>
